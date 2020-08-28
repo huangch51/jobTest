8f3337f55a28d454a53c6e5ac231501b29fc14df
huangcaihuan <huangcaihuan@52tt.com>
2020-08-28 10:43:33 +0800

    11

---
+++ src/main/java/executor/com/tt/base/Test.java
@@ -0,0 +1,11 @@
+package executor.com.tt.base;
+
+/**
+ * @author huangcaihuan
+ * @Since 2020/8/28
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("new");
+    }
+}
