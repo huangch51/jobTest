72da083e9c99149a5cf35d0a50b3e15e3dc0b385
huangcaihuan <huangcaihuan@52tt.com>
2020-08-28 14:11:24 +0800

    11

--- src/main/java/executor/com/tt/base/Test.java
+++ src/main/java/executor/com/tt/base/Test.java
@@ -10,5 +10,7 @@ public class Test {
         System.out.println("new");
         System.out.println("new");
         System.out.println("new");
+        System.out.println("new1");
+        System.out.println("new1");
     }
 }
